51792131ceadaaa8a023a1ff37bcd9038ac9a7be
intanasputri <148935250+intanasputri@users.noreply.github.com>
2025-12-28 18:09:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for, session, make_response, flash
 from models import DaftarAlumni, DaftarProdi, DaftarFakultas, User
 
@@ -293,5 +294,6 @@ def register():
 
     return render_template('register.html', alumni=alumni)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
